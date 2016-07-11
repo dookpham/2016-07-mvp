e413f55a76dc60f219cf0fce5cdb4c796a73d303
DookPham <dookpham@gmail.com>
2016-07-11 11:02:12 -0700

    Implement index.html and public files

--- server/server.js
+++ server/server.js
@@ -1,6 +1,8 @@
 var express = require('express');
-
+var path = require('path');
 var app = express();
+app.use(express.static(path.join(__dirname, '../public')));
+
 
 var port = 3000;
 app.listen(3000, function(err) {
